d7f82f3a23521d84880d1e946b13b4b20b252279
kuzirh <hrizukmichael@gmail.com>
2022-10-02 20:25:24 -0400

    project should be formatted and ready for constructs

--- src/lib/constructs/lambda.ts
+++ src/lib/constructs/lambda.ts
@@ -1,14 +1,21 @@
 import { Construct } from 'constructs';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
-import { StackProps } from 'aws-cdk-lib';
 
+interface LambdaProps {
+  codePath: string
+}
+require('../../build/functions/helloWorld')
 export class Lambda extends Construct {
-  constructor(scope: Construct, id: string) {
+  constructor(scope: Construct, id: string, props: LambdaProps) {
     super(scope, id);
 
     const lambdaConfig = {
       runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset(props.codePath),
+      handler: 'index.handler'
     };
+
+
     const commonLambda = new lambda.Function(
       this,
       'placeholder-name',
